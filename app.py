dbb0cbe9d292528e18dad4a3aae0c5c02407228e
Alexandr Ivanov <49757368+Kahuord@users.noreply.github.com>
2023-03-16 19:04:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template, request
-#from nologi import calculate_salary, minimum_income
-from taxes import calculate_salary_izv_voz_inv
+from taxes import calculate_salary_izv_voz_inv, value_rabotofatela
 podohod_nalog_salary = 1667
 sos_nalog_rabotafatel = 0.2359
 poslina_riska_rabotofatela = 0.36
 app = Flask(__name__)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def salary_calculator():
@@ -14,15 +14,15 @@ def salary_calculator():
         disability = int(request.form.get('disability'))
         izv = int(request.form.get('izv'))
 
-        salary_Netto =round(calculate_salary_izv_voz_inv(salary, izv, pension, disability),2)
-        nalog_rabotofatela = salary * sos_nalog_rabotafatel
-        employers_expenses =round(salary + nalog_rabotofatela + poslina_riska_rabotofatela, 2)
-        tax_amount =round(salary - salary_Netto, 2)
+        salary_Netto = round(calculate_salary_izv_voz_inv(salary, izv, pension, disability), 2)
+        employers_expenses = round(value_rabotofatela(salary, pension), 2)
+        tax_amount = round(salary - salary_Netto, 2)
 
         return render_template('result.html', salary=salary, salary_Netto=salary_Netto, tax_amount=tax_amount,
                                employers_expenses=employers_expenses)
     else:
         return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
